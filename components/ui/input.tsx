a0fcae4727f0aff42bf371574d9e4488bccc7f1a
张骏豪 <3316872395@qq.coom>
2024-04-30 18:31:16 +0800

    change swap form style

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,22 +1,26 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import {useState} from "react";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      const [lineWidth,setLineWidth] = useState(0);
+    return (<div onFocus={(event)=>setLineWidth(event.target.offsetWidth)}
+                 onBlur={(e)=>setLineWidth(0)}>
+            <input type={type}
+                className={cn(
+                    "flex h-10 w-full  px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+                    className
+                )}
+                ref={ref}
+                {...props}
+            />
+            <div className="h-px w-0 mx-auto ease-out duration-700 bg-line shadow-2xl" style={{width:`${lineWidth}px`}}/>
+    </div>
     )
   }
 )
